337950970779c479068d6b8337fe4dafa351775d
Andres Sikk <git@github.com>
2025-10-16 18:36:23 +0300

    Add files

---
+++ Async_Sleep_Console_1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Async_Sleep_Console_1
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            int tick = 1;
+            int amount = 5;
+            int delay = 1000;
+            while (tick < 3600)
+            {
+                Console.WriteLine(tick);
+                await Task.Delay(delay);
+                if (tick % amount == 0)
+                {
+                    Console.Clear();
+                }
+                tick++;
+            }
+        }
+    }
+}
